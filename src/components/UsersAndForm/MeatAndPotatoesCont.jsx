3964964af7205b43b6ab14f82ad3b584ec4e6906
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-22 19:50:38 -0600

    'Bleh'

--- src/components/UsersAndForm/MeatAndPotatoesCont.jsx
+++ src/components/UsersAndForm/MeatAndPotatoesCont.jsx
@@ -1,68 +1,47 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 import UsersCont from './UsersCont'
 import FormCont from './FormCont'
 
-export default function MeatAndPotatoesCont({ getUsers }) {
+export default function MeatAndPotatoesCont() {
 
-  // const MOCK_API_URL = 'https://650c446e47af3fd22f676202.mockapi.io/users'
-
-  // const [users, setUsers] = useState([{
-  //   address: 'Missoula',
-  //   name: 'TDawg',
-  //   profilePicture: 'https://loremflickr.com/640/480/people',
-  //   state: 'MT',
-  //   id: 1,
-  // }])
-
-  // const [newUserName, setNewUser] = useState('')
-  // // const []
-
-  // console.log(users)
-
-  // function getUsers() {
-  //   fetch(MOCK_API_URL)
-  //     .then(data => data.json())
-  //      .then(data => {
-  //        console.log(data);
-  //      })
-  //     .catch(error => {
-  //       console.log(error)
-  //     })
-  // }
-
-  // useEffect(() => {
-  //   getUsers()
-  // }, [])
-
-  // function deleteUsers(id) {
-  //   fetch(`${MOCK_API_URL}/${id}`, {
-  //     method: 'DELETE',
-  //   }) .then(() => getUsers())
-  // }
-
-  // function postUsers(id) {
+  const [users, setUsers] = useState([{}])
+  const URL = 'https://650c446e47af3fd22f676202.mockapi.io/users'
+  
+  const getUsers = () => {
     
-  //   let data = {
-  //   address: "Owensboro",
-  //   name: "Eleanor Ortiz",
-  //   profilePicture: "https://loremflickr.com/640/480/people",
-  //   state: "Missouri",
-  //   }
-    
-  //   fetch(MOCK_API_URL, {
-  //     method: 'POST', 
-  //     headers: { "Content-Type": "application/json" }, 
-  //     body: JSON.stringify()
-  //   })
-  // }
+    fetch(URL)
+      .then(data => data.json())
+      .then(data => {
+        console.log(data);
+        console.log("API Data:", data);
 
+        setUsers(data)
+      })
+  }
 
   
+  useEffect(() => {
+    getUsers()
+  }, [])  
+
+  const deleteUser = (users) => {
+  }
+
+  const updateUser = (users) => {
+  }
+
+  const postNewUser = (users) => {
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+    }
+  }
 
   return (
     <div className='flex flex-row items-center justify-center w-full h-3/4 bg-cyan-600 mx-auto rounfded-xl'>
-        <UsersCont getUsers={getUsers}/>
-        <FormCont/>    
+        <UsersCont deleteUser={deleteUser}  updateUser={updateUser}/>
+        <FormCont postNewUser={postNewUser}/>    
     </div>
   )
 }
