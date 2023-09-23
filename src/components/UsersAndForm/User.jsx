8e2ebfe68a9cb305ff8254a0fb47674ba1fb3971
trevornei <117675657+trevornei@users.noreply.github.com>
2023-09-22 20:04:29 -0600

    Added delete and update button for each user.

--- src/components/UsersAndForm/User.jsx
+++ src/components/UsersAndForm/User.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 export default function User() {
   return (
-    <div className='flex flex-col w-1/4 h-auto bg-slate-100 rounded-xl p-6'>
+    <div className='flex flex-row w-auto h-auto bg-slate-100 rounded-xl p-6'>
         <div className="p-3">
             Name: 
         </div>
+        <button className='p-2 m-2 rounded-2xl bg-cyan-200'>
+          <h3 className="">
+            Update
+          </h3>
+        </button>
+        <button className='p-2 m-2 rounded-2xl bg-cyan-200'>
+          <h3 className=''>
+            Delete
+          </h3>
+        </button>
     </div>
   )
 }
