0981ba11b015c44a9bf8acbc5e5e48e4db2859a9
trevornei <117675657+trevornei@users.noreply.github.com>
2023-10-05 14:15:59 -0600

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,35 @@ import MeatAndPotatoesCont from './components/UsersAndForm/MeatAndPotatoesCont'
 import RandomImage from './RandomImage'
 import KeyboardCat from './assets/Keyboard_cat.jpg'
 import Nav from './components/UsersAndForm/Nav'
+// import { Outlet, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 
 
 export default function App() {
 
+  // const router = createBrowserRouter(
+  //   createRoutesFromElements(
+  //     <Route path="/" element={<Root />}>
+  //       <Route index element={} />
+  //     </Route>
+  //   )
+  // )
+
+  // const Root = () => {
+  //   return (
+  //     <>
+  //       <Link to="/"></Link>
+  //       <Link to="/"></Link>
+  //       <Link to="/"></Link>
+
+  //       <div>
+  //         <Outlet />
+  //       </div>
+  //     </>
+  //   )
+  // }
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-start mx-3/4 p-12">
+    <div className="w-full h-full flex flex-col items-center justify-start mx-3/4 px-12">
       <Nav/>
       <RandomImage src={KeyboardCat}/>
       <MeatAndPotatoesCont/>
--- src/components/UsersAndForm/Nav.jsx
+++ src/components/UsersAndForm/Nav.jsx
@@ -3,16 +3,15 @@ import React from 'react'
 export default function Nav() {
   return (
     <>
-        <div className="fixed end-0">
-            <ul className='flex flex-row p-6'>
+        <div className="fixed bg-emerald-400 w-full mix-blend-multiply h-auto">            <ul className='flex flex-row float-right p-6 z-10'>
                 <li className='m-6 font-extrabold text-2xl'>
-                    <h3 className="">Home</h3>
+                    <h3 className="tracking-widest">Home</h3>
                 </li>
                 <li className='m-6 font-extrabold text-2xl'>
-                    <h3 className="">About</h3>
+                    <h3 className="tracking-widest">About</h3>
                 </li>
                 <li className='m-6 font-extrabold text-2xl'>
-                    <h3 className="">Contact</h3>
+                    <h3 className="tracking-widest">Contact</h3>
                 </li>
             </ul>
         </div>
