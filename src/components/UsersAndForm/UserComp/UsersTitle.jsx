86ccd1637de5806afe7d88f60001dde3afd21634
trevornei <117675657+trevornei@users.noreply.github.com>
2023-11-15 17:06:34 -0700

    Changed landing page title.

--- src/components/UsersAndForm/UserComp/UsersTitle.jsx
+++ src/components/UsersAndForm/UserComp/UsersTitle.jsx
@@ -4,7 +4,7 @@ export default function UsersTitle() {
   return (
     <>
         <div className="flex flex-col">
-            <h1 className="text-3xl font-extrabold">Users Database :D</h1>
+            <h1 className="text-3xl font-extrabold">Users Database</h1>
         </div>
     </>
   )
