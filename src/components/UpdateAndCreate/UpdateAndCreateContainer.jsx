500f04775e31b37e5c9f3651ef67e03ccda73545
trevornei <117675657+trevornei@users.noreply.github.com>
2023-11-15 17:50:09 -0700

    Changed the name of an import for logical consitency. Please refer to Phil Karlton.

--- src/components/UpdateAndCreate/UpdateAndCreateContainer.jsx
+++ src/components/UpdateAndCreate/UpdateAndCreateContainer.jsx
@@ -5,7 +5,7 @@ import FormCont from '../UsersAndForm/FormFormMrof/FormCont'
 export default function UpdateAndCreateContainer({ updateUser, setSelectedId, setUpdatedName, setUpdatedCompany, postNewUser,setNewName, setNewCompany }) {
   return (
     <>
-        <div className="flex flex-row">
+        <div className="flex flex-row my-16">
             <UpdateUserCont updateUser={updateUser} setSelectedId={setSelectedId} setUpdatedName={setUpdatedName} setUpdatedCompany={setUpdatedCompany}/>
             <FormCont postNewUser={postNewUser} setNewName={setNewName} setNewCompany={setNewCompany}/>
         </div>
--- src/components/UsersAndForm/FormFormMrof/FormCont.jsx
+++ src/components/UsersAndForm/FormFormMrof/FormCont.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Form from './PostUserForm'
+import PostUserForm from './PostUserForm'
 
 export default function FormCont({ postNewUser, setNewName, setNewCompany }) {
   return (
     <div className='flex flex-col flex-wrap items-center justify-center w-full h-full bg-cyan-100'>
-        <Form postNewUser={postNewUser} setNewName={setNewName} setNewCompany={setNewCompany} />
+        <PostUserForm postNewUser={postNewUser} setNewName={setNewName} setNewCompany={setNewCompany} />
     </div>
   )
 }
